458b1975422d1a3c853ec67897af5bd09abd529e
hegnemurd <hegne.murd@gmail.com>
2021-04-22 19:44:51 +0100

    Running Unit and End to End Tests

--- beginner/angular-cli-by-john-papa/angular-routing/src/app/admin/users/users.component.ts
+++ beginner/angular-cli-by-john-papa/angular-routing/src/app/admin/users/users.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  users: any[];
+  hasPremission = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.hasPremission) {
+      this.getUsers()
+        .then((users) => (this.users = users))
+        .catch((e) => console.log(e.message));
+    } else {
+      this.users = [];
+    }
   }
 
+  async getUsers() {
+    return [
+      { name: 'John', email: 'john@doe.com' },
+      { name: 'Jane', email: 'jane@doe.com' },
+    ];
+  }
 }
