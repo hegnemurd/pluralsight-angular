79555e2d58a9782bd4a5fa02e05a62ca9b3a46e7
hegnemurd <hegne.murd@gmail.com>
2021-04-30 21:16:12 +0100

    Scalable, Maintainable CSS/SCSS Architecture in Angular

--- beginner/styling-angular-applications-by-brian-treese/scalable-maintainable-css-scss-architecture-in-angular/src/app/app.module.ts
+++ beginner/styling-angular-applications-by-brian-treese/scalable-maintainable-css-scss-architecture-in-angular/src/app/app.module.ts
@@ -1,43 +1,44 @@
-import { NgModule }                 from '@angular/core';
-import { BrowserModule }            from '@angular/platform-browser';
-import { AppComponent }             from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 // Content Components
-import { AccordionComponent }       from './shared/components/content/accordion/accordion.component';
-import { AccordionPaneComponent }   from './shared/components/content/accordion/accordion-pane/accordion-pane.component';
-import { TabsComponent }            from './shared/components/content/tabs/tabs.component';
-import { TabsPaneComponent }        from './shared/components/content/tabs/tabs-pane/tabs-pane.component';
+import { AccordionComponent } from './shared/components/content/accordion/accordion.component';
+import { AccordionPaneComponent } from './shared/components/content/accordion/accordion-pane/accordion-pane.component';
+import { TabsComponent } from './shared/components/content/tabs/tabs.component';
+import { TabsPaneComponent } from './shared/components/content/tabs/tabs-pane/tabs-pane.component';
 
 // Navigation Components
-import { NavBarPrimaryComponent }   from './shared/components/navigation/navbar-primary/navbar-primary.component';
+import { NavBarPrimaryComponent } from './shared/components/navigation/navbar-primary/navbar-primary.component';
 import { NavBarSecondaryComponent } from './shared/components/navigation/navbar-secondary/navbar-secondary.component';
-import { PillsComponent }           from './shared/components/navigation/pills/pills.component';
+import { PillsComponent } from './shared/components/navigation/pills/pills.component';
 
 // PopUp Components
-import { 
+import {
   ModalComponent,
-  ModalContentDirective }           from './shared/components/popups/modal/modal.component';
+  ModalContentDirective,
+} from './shared/components/popups/modal/modal.component';
 import {
   ToolTipComponent,
-  TooltipMessageDirective }         from './shared/components/popups/tooltip/tooltip.component';
+  TooltipMessageDirective,
+} from './shared/components/popups/tooltip/tooltip.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ 
-      AppComponent,
-      AccordionComponent,
-      AccordionPaneComponent,
-      TabsComponent,
-      TabsPaneComponent,
-      NavBarPrimaryComponent,
-      NavBarSecondaryComponent,
-      PillsComponent,
-      ModalComponent,
-      ModalContentDirective,
-      ToolTipComponent,
-      TooltipMessageDirective
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    AccordionComponent,
+    AccordionPaneComponent,
+    TabsComponent,
+    TabsPaneComponent,
+    NavBarPrimaryComponent,
+    NavBarSecondaryComponent,
+    PillsComponent,
+    ModalComponent,
+    ModalContentDirective,
+    ToolTipComponent,
+    TooltipMessageDirective,
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
